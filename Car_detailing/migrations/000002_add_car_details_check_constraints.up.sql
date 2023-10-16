ba00c98ad3bee7ef6648456ad85739bed3c62c49
21B030702 <98741480+21B030702@users.noreply.github.com>
2023-10-16 19:25:33 +0600

    Add files via upload

--- Car_detailing/migrations/000002_add_car_details_check_constraints.up.sql
+++ Car_detailing/migrations/000002_add_car_details_check_constraints.up.sql
@@ -1,3 +1,2 @@
 ALTER TABLE car_details ADD CONSTRAINT car_details_weight_check CHECK (weight > 0);
-ALTER TABLE car_details ADD CONSTRAINT car_details_date_of_production_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
-ALTER TABLE car_details ADD CONSTRAINT car_details_price_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
+ALTER TABLE car_details ADD CONSTRAINT car_details_price_length_check CHECK (price > 0);
